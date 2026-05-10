2f686c2fa829cbfce03361f9acd6ff80bf9d2839
pooya-rostami <36406763+pooya-rostami@users.noreply.github.com>
2026-05-10 10:29:22 +0000

    Deploying to gh-pages from @ pooya-rostami/pooya-rostami.github.io@2f1c8553ac5eef462f5ed79836511fff17c75526 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,6 +59,9 @@ ninja.data = [{
           section: "News",},{id: "news-invited-as-a-pc-member-belgium-netherlands-software-evolution-workshop-benevol-2026",
           title: 'Invited as a PC member @ Belgium-Netherlands Software Evolution Workshop (BENEVOL 2026)',
           description: "",
+          section: "News",},{id: "news-i-am-delighted-to-announce-that-i-have-successfully-defended-my-phd-thesis-and-officially-obtained-my-phd-degree",
+          title: 'I am delighted to announce that I have successfully defended my PhD thesis...',
+          description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
@@ -86,7 +89,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%70%6F%6F%79%61.%72%6F%73%74%61%6D%69.%6D@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%70%6F%6F%79%61[%64%6F%74]%72%6F%73%74%61%6D%69[%64%6F%74]%6D[%61%74]%67%6D%61%69%6C[%64%6F%74]%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
@@ -115,6 +118,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=FRhphAoAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-scopus',
+        title: 'Scopus',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.scopus.com/authid/detail.uri?authorId=58141890000", "_blank");
         },
       },{
       id: 'light-theme',
